f7c6927261139d1a56a344eb4d9e49c3110e5629
nicotriballier <nicolas.triballier@gmail.com>
2025-10-05 01:09:12 -0700

    build

--- .next/static/chunks/app_page_tsx_89f5f50e._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/app_components_MapComponent_tsx_74593430._.js",
-  "static/chunks/_22d16039._.js"
-],
-    source: "dynamic"
-});
