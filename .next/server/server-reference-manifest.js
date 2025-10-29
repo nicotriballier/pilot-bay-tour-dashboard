4ddcb769078c328a31df7333bccc15e1a629be29
nicotriballier <nicolas.triballier@gmail.com>
2025-10-29 11:26:46 -0700

    Remove .next/ directory from git tracking

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"HXu/k4FYOxJIju0xbS1Fp/3HPofHMf7Acp5ynoo/r+k=\"\n}"
--- .next/static/chunks/app_page_tsx_89f5f50e._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/app_components_MapComponent_tsx_c9068368._.js",
-  "static/chunks/_1d53f345._.js"
-],
-    source: "dynamic"
-});
